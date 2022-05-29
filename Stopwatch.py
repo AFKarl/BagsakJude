6cc96ebbce5ce0fe66016b61a4ef6899f02cac38
AFKarl <105416073+AFKarl@users.noreply.github.com>
2022-05-29 23:16:39 +0800

    Update Stopwatch.py

--- Stopwatch.py
+++ Stopwatch.py
@@ -30,55 +30,41 @@ class stopwatch(tk.Frame):
         self.lap_5 = tk.Label(text="00:00:00", font=("Arial", 15), height="1")
         self.lap_5.pack()
 
-        self.start_time_button = tk.Button(text='Start', height=2, width=10, font=('Arial', 15),)
+        self.start_time_button = tk.Button(text='Start', height=2, width=10, font=('Arial', 15), command=self.start_time)
         self.start_time_button.pack(anchor='s', side=tk.LEFT)
 
-        self.pause_time_button = tk.Button(text='Stop', height=2, width=11, font=('Arial', 15), )
+        self.pause_time_button = tk.Button(text='Stop', height=2, width=11, font=('Arial', 15), command=self.pause_time)
         self.pause_time_button.pack(anchor='s', side=tk.LEFT)
 
-        self.split_time_button = tk.Button(text='Splits/Laps', height=2, width=11, font=('Arial', 15),)
+        self.split_time_button = tk.Button(text='Splits/Laps', height=2, width=11, font=('Arial', 15), command=self.split_time)
         self.split_time_button.pack(anchor='s', side=tk.LEFT)
 
-        self.reset_button = tk.Button(text='Reset', height=2, width=10, font=('Arial', 15),)
+        self.reset_button = tk.Button(text='Reset', height=2, width=10, font=('Arial', 15), command=self.reset_time)
         self.reset_button.pack(anchor='s', side=tk.LEFT)
 
         self.window.title("STOPWATCH")
-        
-def split_time(self):
 
+
+    def split_time(self):
         if self.running:
-
             self.lap_dictionary = {
-
                 "0": self.lap_1,
-
                 "1": self.lap_2,
-
                 "2": self.lap_3,
-
                 "3": self.lap_4,
-
                 "4": self.lap_5,
-
             }
-
             lap_length = [x for x in range(len(self.lap_dictionary))]
 
             if  self.laps in lap_length:
-
                 self.lap_dictionary[str(self.laps)].configure(text=self.stopwatch_label['text'])
-
                 self.laps += 1
 
             else:
-
                 self.laps = 0
-
                 for key, value in self.lap_dictionary.items():
-
                     value.configure(text="00:00:00")
-
-                self.lap_dictionary[str(self.laps)].configure(text=self.stopwatch_label['text']
+                self.lap_dictionary[str(self.laps)].configure(text=self.stopwatch_label['text'])
 
 
 root = tk.Tk()
